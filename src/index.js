8327b46401d0dd3a88255134b64f0eb789aa72ad
kiddhustle <kiddhustle@wiardweb.com>
2019-09-09 17:51:49 +0100

    add widget api

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App sort="gold" id="medalsapp"/>, document.getElementById('root'));
+window.widget = {
+  initialize: function (container, sortBy) {
+    ReactDOM.render(<App sort={sortBy}/>, document.getElementById(container));
+  }
+}
+
+window.widget.initialize('root', 'gold')
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
