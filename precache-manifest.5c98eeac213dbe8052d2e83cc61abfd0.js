29e7788e95b3a9c2f96ad95d20ed151026be8da0
kiddhustle <kiddhustle@wiardweb.com>
2019-09-09 18:20:37 +0100

    Updates

---
+++ precache-manifest.5c98eeac213dbe8052d2e83cc61abfd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedd1ac5c2646f1c7c735622fa49d984",
+    "url": "/thomson-reuters-test/index.html"
+  },
+  {
+    "revision": "fca738c7421c2ce2b970",
+    "url": "/thomson-reuters-test/static/css/main.4a3f3e36.chunk.css"
+  },
+  {
+    "revision": "78e264dd1ff938f62b63",
+    "url": "/thomson-reuters-test/static/js/2.aef7f414.chunk.js"
+  },
+  {
+    "revision": "fca738c7421c2ce2b970",
+    "url": "/thomson-reuters-test/static/js/main.57db4a1d.chunk.js"
+  },
+  {
+    "revision": "2b97225d3c8429cd4cfe",
+    "url": "/thomson-reuters-test/static/js/runtime~main.8bec5e0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thomson-reuters-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
