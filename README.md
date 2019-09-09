a19df768a551574ffc6394e2c029a4c4a6c1d88c
kiddhustle <kiddhustle@wiardweb.com>
2019-09-09 15:41:26 +0100

    add thomson reuters test files

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+# Medal Count Widget
+The widget is meant to be embedded on client’s websites during the Olympic
+games. The widget shows up to 10 countries that have won the most medals of a
+given kind in the Olympics games. It has the following requirements:
+
+* It can accept up to two parameters:
+* element_id – an id of the element that the module will use as its container.
+* sort - determines the type of medals to sort by. It can have one of the following values: 'total', 'gold', 'silver' and 'bronze'.
+
+
+* If no sort parameter is passed the sort should be by gold medals.
+* When ranking by total medals, ties are broken by most gold.
+* When ranking by gold, ties are broken by most silver.
+* When ranking by silver, ties are broken by most gold.
+* When ranking by bronze, ties are broken by most gold.
+* Users can click the column headers to re-sort the countries by gold, silver,
+bronze, or total medals won. With each sort the possibility exists that new
+countries will enter and depart the top 10. The medals data should not be
+re-fetched from the server when sort changes.
+* Please follow the design shown in the screenshots on the following pages.
+A sprite with the 13 countries flags (in alphabetical order according to their
+
+country code) can be found here: https://s3-us-west-2.amazonaws.com/reuters.medals-widget/flags.png
+
+To get the list of countries and their medals use the following URL: https://s3-us-west-2.amazonaws.com/reuters.medals-widget/medals.json.
+
+Note that this
+
+should be loaded dynamically using an Ajax call (simulate a call to a live API endpoint). Give an appropriate error message if there was an error retrieving the JSON data (if in real production scenario the API is down, you can simulate that by
+running the widget but turning off your wifi/internet connection). Also note that although this is hosted on S3, a CORS file was set on this folder and thus this data can be retrieved with a regular Ajax request.
+We prefer that the solution will be implemented using react or if you are familiar
+with ClojureScript you can submit it as ClojureScript code using reagent library as
+the React wrapper.
+If you’re using other libraries please explain briefly why you chose them. Your
+code can be on github (preferred), or emailed back as a zip file containing all the
+assets needed (html, css, js). If you use a zip zip file please email it to:
+talgiat@gmail.com.
+The following pages contain screenshots of the widget in different states.
+
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
 import React from 'react';
 import logo from './logo.svg';
+import flags from './flags.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DATA_URI = 'https://s3-us-west-2.amazonaws.com/reuters.medals-widget/medals.json'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    const {sort} = props
+    this.state = {
+      sortBy: sort ? sort : 'gold',
+      errorLoading: false,
+      isLoading: true,
+      errorMessage: '',
+      data: undefined
+    }
+
+    // bind methods
+    this.onClickSortHeader = this.onClickSortHeader.bind(this)
+  }
+
+   async componentDidMount() {
+     try {
+       const res = await fetch(DATA_URI)
+       let data = await res.json()
+       data = data.map((country) => {
+         // Create total property
+         country.total = country.gold + country.silver + country.bronze
+         return country
+       })
+
+       this.setState({
+         data,
+         isLoading: false
+       })
+     } catch(e) {
+       this.setState({errorLoading: true})
+     }
+
+   }
+
+  getSortedData (data, sortBy) {
+    data = data != null ? data : this.state.data
+    const clonedData = JSON.parse(JSON.stringify(data))
+
+    clonedData.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) { return 1}
+      if (a[sortBy] > b[sortBy]) { return -1}
+      if(sortBy === 'gold') {
+        if (a['silver'] < b['silver']) { return 1}
+        if (a['silver'] > b['silver']) { return -1}
+      } else {
+        if (a['gold'] < b['gold']) { return 1}
+        if (a['gold'] > b['gold']) { return -1}
+      }
+      return 0
+      // return b[sortBy] - a[sortBy]
+    })
+
+    return clonedData
+  }
+  onClickSortHeader (e) {
+    const {target} = e
+    const el = target.hasAttribute('data-sort-type') ? target : target.closest('[data-sort-type]')
+    const sort = el.getAttribute('data-sort-type')
+    const data = this.getSortedData(null, sort)
+
+    this.setState({
+      data,
+      sortBy: sort
+    })
+  }
+
+  render () {
+    const {id, sort} = this.props
+    const {isLoading, errorLoading, data, sortBy} = this.state
+
+    if(isLoading ) {
+      return (<p>Currently loading..</p>)
+    }
+    if(errorLoading) {
+      return (<p>Error loading results. Please contact a site admin</p>)
+    } else {
+      return (
+        <div className="App" id={id} sort={sort}>
+        <h2 className="medalsheader">Medal Count</h2>
+          <table className="medalstable">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th
+                  scope="col" data-sort-type="gold"
+                  className={sortBy === 'gold' ? 'sorted': null}
+                  onClick={this.onClickSortHeader}
+                  title="Click to sort by Gold medals"
+                >
+                  <span className="medal-icon medal-icon--gold" />
+                </th>
+                <th
+                  scope="col" data-sort-type="silver"
+                  className={sortBy === 'silver' ? 'sorted': null}
+                  onClick={this.onClickSortHeader}
+                  title="Click to sort by Silver medals"
+                  >
+                    <span className="medal-icon medal-icon--silver" />
+                  </th>
+                <th
+                  scope="col" data-sort-type="bronze"
+                  className={sortBy === 'bronze' ? 'sorted': null}
+                  onClick={this.onClickSortHeader}
+                  title="Click to sort by Bronze medals"
+                  >
+                    <span
+                      className="medal-icon medal-icon--bronze" />
+                  </th>
+                <th scope="col" data-sort-type="total"
+                className={sortBy === 'total' ? 'sorted': null}
+                onClick={this.onClickSortHeader}
+                title="Click to sort by Total medals"
+                >TOTAL</th>
+              </tr>
+            </thead>
+            <tbody>
+            {data && this.getSortedData(data, sortBy).slice(0, 10).map((country, i) => (
+              <tr key={country.code}>
+                <td>{i + 1}</td>
+                <td className="medalstable__country">
+                  <span className={`flag flag--${country.code}`} /> {country.code}
+                </td>
+                <td>{country.gold}</td>
+                <td>{country.silver}</td>
+                <td>{country.bronze}</td>
+                <td><strong>{country.total}</strong></td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
+
 }
 
 export default App;
